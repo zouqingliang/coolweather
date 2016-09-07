7afe5f70ca61d5976e7457740a35535c87146288
Lenly <liangzai203@163.com>
2016-09-07 14:33:46 +0800

    First commit

---
+++ app/src/main/java/com/liang/pro/coolweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.liang.pro.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
