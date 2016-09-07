dbbf9042b52a3657b0686481f88763e9846bb416
Lenly <liangzai203@163.com>
2016-09-07 23:03:03 +0800

    完成遍历地区功能

--- app/src/main/java/com/liang/pro/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/liang/pro/coolweather/activity/MainActivity.java
@@ -1,15 +1,231 @@
 package com.liang.pro.coolweather.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.liang.pro.coolweather.R;
+import com.liang.pro.coolweather.db.CoolWeatherDB;
+import com.liang.pro.coolweather.model.City;
+import com.liang.pro.coolweather.model.County;
+import com.liang.pro.coolweather.model.Province;
+import com.liang.pro.coolweather.util.HttpCallbackListener;
+import com.liang.pro.coolweather.util.HttpUtil;
+import com.liang.pro.coolweather.util.Utility;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
+
+    private static final String KEY = "c1b7dca040c858f28407acef96b0cf03";
+
+    private ProgressDialog progressDialog;
+    private TextView tvTitle;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private CoolWeatherDB coolWeatherDB;
+    private List<String> dataList = new ArrayList<String>();
+
+    private List<Province> provinceList; //省列表
+    private List<City> cityList;   //市列表
+    private List<County> countyList;    //县列表
+
+    private Province selectedProvince;  //选中的省份
+    private City selectedCity;    //选中的城市
+    private int currentLevel;     //当前选中的级别
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        initView();
+        bindData();
+    }
+
+    public void initView(){
+        listView = (ListView) findViewById(R.id.list_view);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+    }
+
+    public void bindData(){
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+
+        coolWeatherDB = CoolWeatherDB.getInstance(this);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (currentLevel == LEVEL_PROVINCE){
+                    selectedProvince = provinceList.get(i);
+                    queryCities();
+                } else if (currentLevel == LEVEL_CITY){
+                    selectedCity = cityList.get(i);
+                    queryCounties();
+                }
+            }
+        });
+        queryProvinces(); //加载省级数据
+    }
+
+    /**
+     * 查询全国所有的省，优先从数据库查询，如果没有查询到在去服务器上查询
+     */
+    private void queryProvinces() {
+        provinceList = coolWeatherDB.loadProvinve();
+        if (provinceList.size() > 0){
+            dataList.clear();
+            for (Province province : provinceList){
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            tvTitle.setText("中国");
+            currentLevel = LEVEL_PROVINCE;
+        } else {
+            queryFromServer(null,"province");
+        }
+    }
+
+    /**
+     * 查询选中的省内所有的市，优先从数据库查询，如果没有查询到再去服务器上查询
+     */
+    private void queryCities() {
+        cityList = coolWeatherDB.loadCity();
+        if (cityList.size() > 0){
+            dataList.clear();
+            for (City city : cityList){
+                dataList.add(city.getCityName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            tvTitle.setText(selectedProvince.getProvinceName());
+            currentLevel = LEVEL_CITY;
+        } else {
+            queryFromServer(selectedProvince.getProvinceCode(),"city");
+        }
+    }
+
+    /**
+     * 查询选中的市内所有的县，优先从数据库中查询，如果没有再到服务器上查询
+     */
+    private void queryCounties() {
+        countyList = coolWeatherDB.loadCounty();
+        if (countyList.size() > 0){
+            dataList.clear();
+            for (County county : countyList){
+                dataList.add(county.getCountyName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            tvTitle.setText(selectedCity.getCityName());
+            currentLevel = LEVEL_COUNTY;
+        } else {
+            queryFromServer(selectedCity.getCityCode(),"county");
+        }
+    }
+
+    /**
+     * 根据传入的代号和类型从服务器上查询省市县数据
+     * @param code
+     */
+    private void queryFromServer(final String code, final String type) {
+        String address;
+        if (!TextUtils.isEmpty(code)){
+            address = "http://www.weather.com.cn/data/list3/city" + code + ".xml";
+        } else {
+            address = "http://www.weather.com.cn/data/list3/city.xml";
+        }
+        showProgressDialog();
+        HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+            @Override
+            public void onFinish(String response) {
+                boolean result = false;
+                if ("province".equals(type)){
+                    result = Utility.handleProvincesResponse(coolWeatherDB,response);
+                } else if ("city".equals(type)){
+                    result = Utility.handleCitiesResponse(coolWeatherDB,response,selectedProvince.getId());
+                } else if ("county".equals(type)){
+                    result = Utility.handleCountiesResponse(coolWeatherDB,response,selectedCity.getId());
+                }
+
+                if (result){  //通过runOnUiThread()方法回到主线程处理逻辑
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if ("province".equals(type)){
+                                queryProvinces();
+                            } else if ("city".equals(type)){
+                                queryCities();
+                            } else if ("county".equals(type)){
+                                queryCounties();
+                            }
+                        }
+                    });
+
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(MainActivity.this,"加载失败",Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+    }
+
+
+    /**
+     * 显示进度对话框
+     */
+    private void closeProgressDialog() {
+        if(progressDialog == null){
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("正在加载中。。。");
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+        progressDialog.show();
+    }
+
+    private void showProgressDialog() {
+        if (progressDialog != null){
+            progressDialog.dismiss();
+        }
+    }
+
+    /**
+     * 捕获back按键，根据当前的级别来判断此时应该返回市列表，省列表，还是直接退出
+     */
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (currentLevel == LEVEL_COUNTY){
+            queryCities();
+        } else if (currentLevel == LEVEL_CITY){
+            queryProvinces();
+        } else {
+            finish();
+        }
     }
 }
